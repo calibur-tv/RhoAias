786b832677cb50a6a5162d6e69e57b535d10c28d
falstack <icesilt@outlook.com>
2017-11-22 08:08:32 +0800

    update deploy script

--- pm2.config.js
+++ pm2.config.js
@@ -7,6 +7,9 @@ module.exports = {
     env: {
       'NODE_ENV': 'development'
     },
+    env_staging: {
+      'NODE_ENV': 'staging'
+    },
     env_production: {
       'NODE_ENV': 'production'
     }
