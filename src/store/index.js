ee63ebd2a6fa0cd14798f68d1ce6ad7081c0e31f
falstack <icesilt@outlook.com>
2017-11-24 08:11:29 +0800

    fixed state

--- src/store/index.js
+++ src/store/index.js
@@ -5,18 +5,42 @@ Vue.use(Vuex)
 
 export function createStore () {
   return new Vuex.Store({
-    state () {
-      return {
-        user: null,
-        ssrContext: null
-      }
+    state: {
+      user: null,
+      token: '',
+      login: false
     },
     mutations: {
-      SET_SSR_CTX (state, data) {
-        state.ssrContext = data
+      SET_USER (state, user) {
+        state.user = user
+        state.login = true
+      },
+      SET_TOKEN (state, data) {
+        state.token = data
       }
     },
-    actions: {},
+    actions: {
+      async init ({ commit }, { ctx }) {
+        const cookie = ctx.header.cookie
+        if (cookie) {
+          let token = ''
+          cookie.split('; ').forEach(item => {
+            const temp = item.split('=')
+            if (temp[0] === 'JWT-TOKEN') {
+              token = temp[1]
+            }
+          })
+          if (token) {
+            const api = new UserApi(token)
+            const user = await api.getLoginUser()
+            if (user) {
+              commit('SET_TOKEN', token)
+              commit('SET_USER', user)
+            }
+          }
+        }
+      }
+    },
     getters: {},
     modules: {}
   })
