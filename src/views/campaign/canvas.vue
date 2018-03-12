96e9344f35793fe917f911b3e23f0399832e9528
falstack <icesilt@outlook.com>
2018-03-12 08:02:26 +0800

    add: campaign routes

---
+++ src/views/campaign/canvas.vue
@@ -0,0 +1,43 @@
+<style lang="scss">
+  #campaign-canvas {
+
+  }
+</style>
+
+<template>
+  <div id="campaign-canvas">
+    campaign canvas
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'campaign-canvas',
+    components: {
+
+    },
+    props: {
+
+    },
+    watch: {
+
+    },
+    computed: {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    created () {
+
+    },
+    methods: {
+
+    },
+    mounted () {
+
+    }
+  }
+</script>
