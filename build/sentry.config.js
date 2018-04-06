ca1bb44abc222aede7a53a39f50e04c44835c26d
falstack <icesilt@outlook.com>
2018-04-06 11:00:49 +0800

    update: deploy

--- build/sentry.config.js
+++ build/sentry.config.js
@@ -1,8 +1,8 @@
 const config = require('../.env').sentry
 
 module.exports = {
-  url: 'https://sentry.io/api/0/projects/',
-  org: 'sentry',
+  url: 'https://sentry.io/',
+  org: 'falstack',
   include: /\.js(\.map)?$/,
   project: 'RhoAias',
   token: config.token
