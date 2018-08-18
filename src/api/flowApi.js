40170396ab60479ac32ed8f8664663ee21a66832
falstack <icesilt@outlook.com>
2018-08-18 10:56:14 +0800

    update: http status code 423

--- src/api/flowApi.js
+++ src/api/flowApi.js
@@ -2,13 +2,13 @@ import BaseApi from "./_baseApi";
 
 export default class extends BaseApi {
   fetch({ sort, type, take, page, minId, seenIds, bangumiId, userZone }) {
-    return this.http.get("flowlist", {
+    return this.http.get("flow/list", {
       params: { sort, type, take, page, minId, seenIds, bangumiId, userZone }
     });
   }
 
   meta({ type }) {
-    return this.http.get("trending/meta", {
+    return this.http.get("flow/meta", {
       params: { type }
     });
   }
