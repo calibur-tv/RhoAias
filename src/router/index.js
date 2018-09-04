212677a54f912d823f0e96f0c8920eaa57065621
falstack <icesilt@outlook.com>
2018-09-04 11:06:26 +0800

    fixed: iOS page scroll

--- src/router/index.js
+++ src/router/index.js
@@ -15,10 +15,26 @@ export function createRouter() {
     mode: "history",
     base: "/",
     scrollBehavior(to, from, savedPosition) {
+      // savedPosition 只有在 popstate 导航（如按浏览器的返回按钮）时可以获取。
       if (savedPosition) {
         return savedPosition;
       } else {
-        return { x: 0, y: 0 };
+        let position = {};
+        // 目标页面子组件少于两个
+        if (to.matched.length < 2) {
+          // 滚动至页面顶部
+          position = { x: 0, y: 0 };
+        } else if (
+          to.matched.some(r => r.components.default.options.scrollToTop)
+        ) {
+          // 如果目标页面子组件中存在配置了scrollToTop为true
+          position = { x: 0, y: 0 };
+        }
+        // 如果目标页面的url有锚点,  则滚动至锚点所在的位置
+        if (to.hash) {
+          position = { selector: to.hash };
+        }
+        return position;
       }
     },
     routes: [
