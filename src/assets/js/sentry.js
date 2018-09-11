dbcfc270123544162d0d1d4a390f69351a4bce32
falstack <icesilt@outlook.com>
2018-09-11 21:51:39 +0800

    update: sentry

--- src/assets/js/sentry.js
+++ src/assets/js/sentry.js
@@ -47,7 +47,8 @@ export default class {
           /(readonly|redefine) property/,
           "Failed to execute 'appendChild' on 'Node'",
           "value.hasOwnProperty is not a function",
-          /([native code]|<anonymous>)/
+          "anonymous",
+          "native code"
         ]
       })
         .addPlugin(RavenVue, Vue)
