a2c39e242b14337dd8f32b9ed62a31820bb89668
falstack <icesilt@outlook.com>
2018-10-05 13:11:21 +0800

    fixed: first sign dialog show

--- src/components/drawers/FirstSignDrawer.vue
+++ src/components/drawers/FirstSignDrawer.vue
@@ -96,14 +96,21 @@ export default {
   },
   mounted() {
     if (!this.$cookie.get("display-first-sign-dialog")) {
-      this.show = true;
-      const id = this.$backdrop.show({
-        click: () => {
-          this.show = false;
-          this.$backdrop.hide(id);
+      try {
+        if (!localStorage.getItem("display-first-sign-dialog")) {
+          this.show = true;
+          const id = this.$backdrop.show({
+            click: () => {
+              this.show = false;
+              this.$backdrop.hide(id);
+            }
+          });
+          this.$cookie.set("display-first-sign-dialog", 1, {
+            expires: 7
+          });
+          localStorage.setItem("display-first-sign-dialog", 1);
         }
-      });
-      this.$cookie.set("display-first-sign-dialog", 1);
+      } catch (e) {}
     }
   }
 };
